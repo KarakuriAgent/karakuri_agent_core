c84de7fa39bc36b25680509204f88f24869cad9c
KoheiYamashita <yamashita@0235.co.jp>
2024-10-14 04:53:32 +0000

    Create viewmodel_providers

--- lib/providers/service_config_screen_viewmodel_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:karakuri_agent/models/service_config.dart';
-import 'package:karakuri_agent/viewmodels/service_config_screen_viewmodel.dart';
-
-final serviceConfigScreenViewmodelProvider = ChangeNotifierProvider.autoDispose
-    .family<ServiceConfigScreenViewmodel, ServiceConfig?>((ref, param) {
-  return ServiceConfigScreenViewmodel(serviceConfig: param);
-});
--- lib/providers/viewmodel_providers.dart
+++ lib/providers/viewmodel_providers.dart
@@ -1,5 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:karakuri_agent/models/service_config.dart';
 import 'package:karakuri_agent/providers/config_storage_provider.dart';
+import 'package:karakuri_agent/viewmodels/service_config_screen_viewmodel.dart';
 import 'package:karakuri_agent/viewmodels/service_settings_screen_viewmodel.dart';
 
 final serviceSettingsScreenViewmodelProvider =
@@ -10,4 +12,9 @@ final serviceSettingsScreenViewmodelProvider =
     await viewModel.build();
   });
   return viewModel;
+});
+
+final serviceConfigScreenViewmodelProvider = ChangeNotifierProvider.autoDispose
+    .family<ServiceConfigScreenViewmodel, ServiceConfig?>((ref, param) {
+  return ServiceConfigScreenViewmodel(serviceConfig: param);
 });

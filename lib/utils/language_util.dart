51fa938fadb74905648bd2e2dba654f57cd332d9
KoheiYamashita <yamashita@0235.co.jp>
2024-10-18 12:01:11 +0000

    Remove LanguageUtil

--- lib/utils/language_util.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-class LanguageUtil {
-    static String get isoLanguageCode {
-    String locale = Platform.localeName;
-    List<String> parts = locale.split('_');
-    return parts[0].split('.')[0];
-  }
-}

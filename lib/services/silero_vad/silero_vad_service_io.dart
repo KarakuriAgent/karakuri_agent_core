a6a37bdc8599051e4f7fe1ec5cf61833d0a61644
KoheiYamashita <yamashita@0235.co.jp>
2024-10-18 13:13:27 +0000

    Add TODO

--- lib/services/silero_vad/silero_vad_service_io.dart
+++ lib/services/silero_vad/silero_vad_service_io.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:karakuri_agent/services/silero_vad/silero_vad_service_interface.dart';
 
+// TODO: Processing for io has not yet been implemented.
 class SileroVadService extends SileroVadServiceInterface {
   @override
   Future<void> create(Function(Uint8List) end) async => {};

1d507324c2eec4662037e24ded2bef1402aa21f8
KoheiYamashita <yamashita@0235.co.jp>
2024-11-08 12:02:50 +0000

    Fix use ServiceException

--- lib/services/image_storage/image_storage_service_web.dart
+++ lib/services/image_storage/image_storage_service_web.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:idb_shim/idb_browser.dart';
 
 import 'package:karakuri_agent/services/image_storage/image_storage_service_interface.dart';
+import 'package:karakuri_agent/utils/exception.dart';
 
 class ImageStorageService extends ImageStorageServiceInterface {
   static const String dbName = 'ImageStorage';
@@ -18,8 +19,7 @@ class ImageStorageService extends ImageStorageServiceInterface {
 
   @override
   Future<void> init() async {
-    if (await getImageNames()
-        .then((keys) => keys.contains(aiRoboImageName))) {
+    if (await getImageNames().then((keys) => keys.contains(aiRoboImageName))) {
       return;
     }
     final ByteData data = await rootBundle.load(assetPath);
@@ -37,7 +37,8 @@ class ImageStorageService extends ImageStorageServiceInterface {
         }
       });
     } catch (e) {
-      throw 'Failed to open database: $e';
+      throw ServiceException(runtimeType.toString(), '_openDatabase',
+          message: 'Failed to open database: $e');
     }
   }
 
@@ -55,7 +56,8 @@ class ImageStorageService extends ImageStorageServiceInterface {
       await transaction.completed;
       db.close();
     } catch (e) {
-      throw 'Failed to save file: $e';
+      throw ServiceException(runtimeType.toString(), 'saveImageZip',
+          message: 'Failed to save file: $e');
     }
   }
 
@@ -71,7 +73,8 @@ class ImageStorageService extends ImageStorageServiceInterface {
 
       return data as List<int>;
     } catch (e) {
-      throw 'Failed to load file: $e';
+      throw ServiceException(runtimeType.toString(), 'getImageZip',
+          message: 'Failed to load file: $e');
     }
   }
 
@@ -87,7 +90,8 @@ class ImageStorageService extends ImageStorageServiceInterface {
 
       return keys.map((key) => key.toString()).toList();
     } catch (e) {
-      throw 'Failed to get keys: $e';
+      throw ServiceException(runtimeType.toString(), 'getImageNames',
+          message: 'Failed to get keys: $e');
     }
   }
 }
